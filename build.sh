ac8108f58e86355901bc1d499d70570ff8c8a654
Jason Rae <jasonrae63@gmail.com>
2020-01-13 13:45:38 -0800

    Make it clear we are building for heroku16

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 # set -x
 set -e
 
-docker build --no-cache --build-arg VIPS_VERSION=${VIPS_VERSION} -t libvips-heroku18 container
+docker build --no-cache --build-arg VIPS_VERSION=${VIPS_VERSION} -t libvips-heroku16 container
 
 mkdir -p build
 
-docker run --rm -t -v $PWD/build:/data libvips-heroku18 sh -c 'cp -f /usr/src/vips/build/*.tar.gz /data/'
+docker run --rm -t -v $PWD/build:/data libvips-heroku16 sh -c 'cp -f /usr/src/vips/build/*.tar.gz /data/'
